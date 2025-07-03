7fd750702e3a738bc0e9fce7826422c39061e8f7
csjallow123 <s1312008@u-aizu.ac.jp>
2025-07-03 19:30:23 +0900

    Won/lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -2,14 +2,21 @@ import java.util.Random;
 
 public class DiceGame {
     public static void main(String[] args) {
-        Random rand = new Random();
-
         System.out.println("Rolling dice...");
+
+        Random rand = new Random();
         int die1 = rand.nextInt(6) + 1;
         int die2 = rand.nextInt(6) + 1;
+        int total = die1 + die2;
 
         System.out.println("Die 1: " + die1);
         System.out.println("Die 2: " + die2);
-        System.out.println("Total value: " + (die1 + die2));
+        System.out.println("Total value: " + total);
+
+        if (total > 7) {
+            System.out.println("You won");
+        } else {
+            System.out.println("You lost");
+        }
     }
 }
